ea05153ecdab7731f456e3d062aa9bf1afd091af
Ryan Mercado <ryan.mercado@madmobile.com>
2026-02-18 11:46:52 -0500

    match terminal bg

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -2,7 +2,10 @@ return {
 	"navarasu/onedark.nvim",
 	priority = 1000,
 	config = function()
-		require('onedark').setup { style = 'darker' }
+		require('onedark').setup { 
+			style = 'darker',  
+			transparent = true,
+		}
 		require('onedark').load()
 	end
 }
